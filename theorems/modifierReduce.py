c2139433934cbb7fe8e3d310eb6ff15180fb9430
wheatwizard <olivee3@rpi.edu>
2016-07-13 13:06:22 -0400

    Improved modifierReduce

--- theorems/modifierReduce.py
+++ theorems/modifierReduce.py
@@ -4,4 +4,11 @@ import re
 def modifierReduce(snippet):
 	snippet = snippet.replace("><","")
 	snippet = snippet.replace("][","")
+	finder = re.compile("[<\[]C*[>\]]")
+	while re.search(finder, snippet):
+		location = re.search(finder, snippet).span()
+		snippet = snippet[:location[0]] + snippet[location[0]+1:location[1]-1] + snippet[location[1]:]
 	return snippet
+
+if __name__ == "__main__":
+	print modifierReduce(clean("(<<>>)"))
