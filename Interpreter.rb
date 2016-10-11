6eda3ba826713248618b74d99b60d0f21da89e7b
wheatwizard <olivee3@rpi.edu>
2016-10-11 15:18:23 -0400

    Fixed a bad character error.

--- Interpreter.rb
+++ Interpreter.rb
@@ -37,7 +37,7 @@ class BrainFlakInterpreter
     # Strip comments
     source = source.gsub(/(^[^#]*)#.*(\n|$)/, '\1')
     # Strips the source of any characters that aren't brackets or part of debug flags
-    @source = source.gsub(/(?<=[()\[\]<>{}])[^@()\[\]<>{}]*/, "")
+    @source = source.gsub(/(?<=^|[()\[\]<>{}])[^@()\[\]<>{}]*/, "")
     @left = Stack.new('Left')
     @right = Stack.new('Right')
     @main_stack = []
