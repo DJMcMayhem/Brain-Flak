cf613e4b6ae51dbba790b5873aef36e552c03e57
Mega-Tom <Mega-Tom@users.noreply.github.com>
2016-11-17 15:35:36 -0600

    Fixed flag issue
    Untested

--- brain_flak.rb
+++ brain_flak.rb
@@ -1,15 +1,14 @@
 require_relative './stack.rb'
 require_relative './Interpreter.rb'
 
-VERSION_STRING =  "Brain-Flak Ruby Interpreter v1.1.1"
+VERSION_STRING =  "Brain-Flak Ruby Interpreter v1.1.2"
 
 require 'optparse'
 
 debug = false
-ascii_in = false
-ascii_out = false
+input_type = :decimal
+output_type = :decimal
 reverse = false
-utf8 = false
 arg_path = ""
 
 parser = OptionParser.new do |opts|
@@ -25,27 +24,28 @@ parser = OptionParser.new do |opts|
     arg_path = file
   end
 
-  opts.on("-a", "--ascii-in", "Take input in ASCII code points and output in decimal. This overrides previous -A and -c flags.") do 
-    ascii_in = true
-    ascii_out = false
+  opts.on("-a", "--ascii-in", "Take input in ASCII code points and output in decimal. This overrides previous -u flags.") do 
+    input_type = :ascii
   end
 
-  opts.on("-A", "--ascii-out", "Take input in decimal and output in ASCII code points. This overrides previous -a and -c flags.") do 
-    ascii_in = false
-    ascii_out = true
+  opts.on("-A", "--ascii-out", "Take input in decimal and output in ASCII code points. This overrides previous -U flags.") do 
+    output_type = :ascii
   end
 
-  opts.on("-c", "--ascii", "Take input and output in ASCII code points, rather than in decimal. This overrides previous -a and -A flags.") do 
-    ascii_in = true
-    ascii_out = true
+  opts.on("-c", "--ascii", "Take input and output in ASCII code points, rather than in decimal. This overrides previous -u and -U flags.") do 
+    input_type = output_type = :ascii
   end
 
   opts.on("-r", "--reverse", "Reverses the order that arguments are pushed onto the stack AND that values are printed at the end.") do
     reverse = true
   end
 
-  opts.on("-u", "--utf8", "Enables handling of UTF-8 characters in input and output") do
-    utf8 = true
+  opts.on("-u", "--utf8", "Enables handling of UTF-8 characters in intput. This overrides previous -a flags.") do
+    input_type = :utf8
+  end
+
+  opts.on("-U", "--utf8", "Enables handling of UTF-8 characters in output. This overrides previous -A flags.") do
+    output_type = :utf8
   end
 
   opts.on("-h", "--help", "Prints info on the command line usage of Brain-Flak and then exits") do
@@ -76,17 +76,17 @@ end
 source_path = ARGV[0]
 if arg_path != "" then
   input_file = File.open(arg_path, 'r')
-  if !ascii_in
+  if input_type == :decimal
     numbers = input_file.read.gsub(/\s+/m, ' ').strip.split(" ")
   else
     numbers = input_file.read.split("")
   end
 
 else
-  if ascii_in
-    numbers = ARGV[1..-1].join(" ").split("")
-  else
+  if input_type == :decimal
     numbers = ARGV[1..-1]
+  else
+    numbers = ARGV[1..-1].join(" ").split("")
   end
 end
 
@@ -99,14 +99,14 @@ source = source_file.read
 source_length = source.length
 
 begin
-  if !ascii_in
+  if input_type == :decimal
     numbers.each do |a|
       raise BrainFlakError.new("Invalid integer in input: \"%s\"" % [a], 0) if !(a =~ /^-?\d+$/)
     end
     numbers.map!(&:to_i)
   else
     numbers.map!(&:ord)
-    if !utf8 then
+    if input_type == :ascii then
       numbers.each do |a|
         raise BrainFlakError.new("Invalid character in input: \"%s\"" % [a.chr(Encoding::UTF_8)], 0) if a > 255
       end
@@ -121,7 +121,7 @@ begin
     unmatched_brak = interpreter.main_stack[0]
     raise BrainFlakError.new("Unclosed '%s' character." % unmatched_brak[0], unmatched_brak[2])
   end
-  interpreter.active_stack.print_stack(ascii_out,reverse,utf8)
+  interpreter.active_stack.print_stack(output_type == :ascii, reverse, output_type == :utf8)
 rescue BrainFlakError => e
   STDERR.puts e.message
 rescue Interrupt
