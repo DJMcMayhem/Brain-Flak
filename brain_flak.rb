78d94c3913ef20c6f3fbcd43407021ad8c25a66e
wheatwizard <olivee3@rpi.edu>
2017-04-18 13:15:59 -0400

    Changed the version string

--- brain_flak.rb
+++ brain_flak.rb
@@ -4,7 +4,7 @@ require_relative './BrainFlueueInterpreter.rb'
 require_relative './ClassicInterpreter.rb'
 require_relative './MiniFlakInterpreter.rb'
 
-VERSION_STRING =  "Brain-Flak Ruby Interpreter v1.4.1-dev"
+VERSION_STRING =  "Brain-Flak Ruby Interpreter v1.4.1"
 
 require 'optparse'
 
